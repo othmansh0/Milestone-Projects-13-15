d7f6a66f80e7a23b6d260be5c77a3b0c4320617c
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-16 10:25:30 +0300

    Setup tableViewController for countries names

--- Milestone-Projects 13-15/ViewController.swift
+++ Milestone-Projects 13-15/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var countries = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Countries"
+        
+        let fm = FileManager.default
+        guard let path = Bundle.main.resourcePath else {return}
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.contains("png"){
+                //removes png from each country
+                //let cs = CharacterSet.init(charactersIn: ".png")
+                countries.append(item)
+                
+            }
+        }
+       
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cs = CharacterSet.init(charactersIn: ".png")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row].trimmingCharacters(in: cs)
+        cell.imageView?.image = UIImage(named: countries[indexPath.row])
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
 
